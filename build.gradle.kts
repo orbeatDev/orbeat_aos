d27eba1422982cf8cb806ec6122fe0c50ca7d98c
wonseok <dnjstjr245@gmail.com>
2022-10-08 13:22:34 +0900

    [CHORE] 모듈간 의존성 설정 (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
@@ -8,9 +7,6 @@ buildscript {
         classpath(Build.androidBuildTools)
         classpath(Build.hiltAndroidGradlePlugin)
         classpath(Build.kotlinGradlePlugin)
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
